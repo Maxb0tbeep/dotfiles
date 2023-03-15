e0d30a9b9c614bf8e230e3546af3e440fb2c99fe
Max <max_cooley@icloud.com>
2023-03-14 18:31:51 -0700

    never know what to write here

--- 001/polybar/launch_polybar.sh
+++ 001/polybar/launch_polybar.sh
@@ -1,7 +1,8 @@
- if type "xrandr"; then
-   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-     MONITOR=$m polybar --reload example &
-   done
- else
-   polybar --reload example &
- fi 
+#!/bin/bash
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi 
